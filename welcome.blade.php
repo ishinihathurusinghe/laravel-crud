60a3af0741fc2c3256ba4fa67338924b23f8e940
Ishini Hathurusinghe <ishinihathurusinghe@gmail.com>
2023-02-23 06:45:03 +0530

    Update welcome.blade.php

--- welcome.blade.php
+++ welcome.blade.php
@@ -1,6 +1,6 @@
-<!DOCTYPE html   >
+<!DOCTYPE html    >
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head> 
+    <head>  
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
